457b18b030499bf7addedba96dc424b7a932620e
Nazar Bezeliuk <nazarfreelance11@gmail.com>
2023-04-25 17:30:08 +0300

    mini-slider

---
+++ src/js/modules/difference.js
@@ -0,0 +1,25 @@
+export default class Diffrence {
+    constructor(oldOfficer, newOfficer, items) {
+        this.oldOfficer = document.querySelector(oldOfficer);
+        this.newOfficer = document.querySelector(newOfficer);
+        this.items = items;
+    }
+
+    hideItems() {
+        this.oldOfficer.querySelectorAll(this.items).array.forEach((item, i, arr) => {
+            if (i !== arr.length - 1) {
+                item.style.display = 'none';
+            }
+        });
+
+        this.newOfficer.querySelectorAll(this.items).array.forEach((item, i, arr) => {
+            if (i !== arr.length - 1) {
+                item.style.display = 'none';
+            }
+        });
+    }
+
+    init() {
+        this.hideItems();
+    }
+}
